382a78210ea1384004ac2d9cab11083a7b797d17
mithrilservice <112543684+mithrilservice@users.noreply.github.com>
2022-08-31 14:10:22 +0530

    Update local.js

--- local.js
+++ local.js
@@ -8,12 +8,23 @@ m.render(root, [
 ])
 
 var count = 0
+var increment = function() {
+  m.request({
+    method: "POST",
+    url: "//rem-rest-api.herokuapp.com/api/tutorial/1",
+    body: {count: count + 1},
+    withCredentials: true.
+  })
+  .then(function(data) {
+    count = parseInt(data.count)
+  })
+}
 
 var Hello = {
   view: function() {
     return m("main", [
       m("h1", "Count function"),
-      m("button", {onclick: function() {count++}}, count + " clicks"),
+      m("button", {onclick: increment}, count + " clicks"),
     ])
   }
 }

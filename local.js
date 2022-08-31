d28a507b7cfaba5f1175cc1c5882692fe3e05c8c
mithrilservice <112543684+mithrilservice@users.noreply.github.com>
2022-08-31 13:35:56 +0530

    Update local.js

--- local.js
+++ local.js
@@ -1,3 +1,8 @@
 var root = document.body
 
-m.render(root, "Hello")
+m.render(root, [
+  m("main", [
+    m("h1", {class: "title"}, "Mithril Service App"),
+    m("button", "This button can be clicked"),
+  ])
+])
